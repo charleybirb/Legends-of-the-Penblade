c12bf4158a5d2894b3874ab1c7468f04dae9c878
charleybirb <charley.birb@gmail.com>
2026-02-26 18:50:52 -0800

    global stats added
    - a global script was added to keep track of player stats like experience, level, strength, etc
    - player gains ep (experience points) when an enemy is killed. the amount of ep they give is saved in their main script
    - also files moved around

---
+++ scripts/enemy/enemy.gd
@@ -0,0 +1,71 @@
+class_name Enemy
+extends CharacterBody3D
+
+const TURN_SPEED := 200
+const MOVE_SPEED := 3
+const ACCELERATION := 4
+const DECELERATION := 10
+const FOLLOW_DISTANCE := 1.0
+const WEIGHT := 1.0
+
+@export var HURTBOX : Area3D
+@export var DETECT_AREA : Area3D
+@export var ANIMATION_PLAYER : AnimationPlayer
+@export var FOCUS_POINT : Marker3D
+@export var HEALTH_COMPONENT : HealthComponent
+
+var health := 10
+var defense := 1
+var ep : int = 1
+var player : CharacterBody3D
+
+func _on_body_detected(body: Node3D) -> void:
+	if body.name != &"Player":
+		return
+	player = body
+	print("player detected")
+	
+
+func _on_body_undetected(body: Node3D) -> void:
+	if body.name != &"Player":
+		return
+	player = null
+
+func _ready() -> void:
+	HEALTH_COMPONENT.connect(&"died", die)
+	HEALTH_COMPONENT.connect(&"damage_taken", update_healthbar)
+	HURTBOX.connect(&"area_entered", Callable(HEALTH_COMPONENT, &"_on_area_entered"))
+	DETECT_AREA.connect("body_entered", _on_body_detected)
+	DETECT_AREA.connect("body_exited", _on_body_undetected)
+
+
+func _physics_process(delta: float) -> void:
+	if player:
+		var to_player := player.global_transform.origin - global_transform.origin
+		var distance := to_player.length()
+		to_player = to_player.normalized()
+		var acceleration := ACCELERATION * (distance - FOLLOW_DISTANCE)
+		velocity = lerp(velocity, to_player * MOVE_SPEED, acceleration * delta)
+		velocity.y = 0
+		var right := global_transform.basis.x
+		var r_dot := to_player.dot(right)
+		
+		rotation_degrees.y += TURN_SPEED * -r_dot * delta
+	else:
+		velocity.x = move_toward(velocity.x, 0, DECELERATION * delta)
+		velocity.z = move_toward(velocity.z, 0, DECELERATION * delta)
+	move_and_slide()
+
+
+func knockback(knockback_strength: float, direction: Vector3) ->  void:
+	velocity = knockback_strength * direction
+
+
+func update_healthbar(curr_health: int, max_health: int) -> void:
+	SignalBus.enemy_health_changed.emit(curr_health, max_health)
+
+
+func die() -> void:
+	Stats.ep += ep
+	print(Stats.ep)
+	queue_free()
---
+++ scripts/global_scripts/stats.gd
@@ -0,0 +1,12 @@
+extends Node
+
+enum Track { DEFENDER, WIZARD, BRUTE }
+
+var strength : int = 3
+var defense : int = 1
+var max_mp : int = 3
+var max_hp : int = 5
+var ep : int = 0 # experience point
+var level : int = 1
+
+var chosen_track : Track
